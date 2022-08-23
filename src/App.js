93e47409ae23d8bf683082bebacfbdf6b02c02a5
Mahesh Vuppala <mahesh.vuppala@icicletech.com>
2022-08-24 01:28:25 +0530

    [branch_changed]Code pushed to main branch

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
+
 const App = () => {
   const [searchField, setSearchField] = useState('');
   const [monsters, setMonsters] = useState([]);
